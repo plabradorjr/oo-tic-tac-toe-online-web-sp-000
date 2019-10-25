c67b923624188ae5bcc5722590a2a5289b018701
Pancho Labrador <plabradorjr@gmail.com>
2019-10-25 23:58:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,6 +2,12 @@ require 'pry'
 
 class TicTacToe
 
+  attr_accessor :board 
+
+  def initialize(board = nil)
+    @board = Array.new(9, " ")
+  end
+
   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -13,22 +19,6 @@ class TicTacToe
     [6,4,2]
   ]
 
-  def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-  end
-
-  def input_to_index(user_input)
-    user_input.to_i - 1
-  end
-
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
-  end
-
-  def turn_count
-    @board.count{|token| token == "X" || token == "O"}
-  end
-
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -36,4 +26,45 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(index, current_player = "x")
+    @board[index] = current_player
+  end
+
+  def position_taken?(index)
+    @board[index] == 'X' || @board[index] == 'O'
+  end
+
+
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken?(index)
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
+
+
+  # def current_player
+  #   turn_count % 2 == 0 ? "X" : "O"
+  # end
+  # 
+  # def turn_count
+  #   @board.count{|token| token == "X" || token == "O"}
+  # end
+  # 
+  
 end
