ad7aa710a21cd3680551d4eecae95157b12be35b
Pancho Labrador <plabradorjr@gmail.com>
2019-10-26 00:36:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,16 +77,16 @@ class TicTacToe
   end
 
   def draw?
-    !won?(@board) && full?(@board)
+    !won? && full?
   end
 
   def over?
-    won?(@board) || draw?(@board)
+    won? || draw?
   end
 
   def winner
-    if won?
-      @board[winning_combo.first]
+    if combo = won?
+      @board[combo.first]
     end
   end
 
